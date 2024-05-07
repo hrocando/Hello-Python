65880909a2c31d4435ab9490361b6de2c324bc6a
Hernan R. Ocando Morales <hrocando@gmail.com>
2024-05-07 16:52:49 -0500

    Add files via upload

---
+++ 12_exceptions.py
@@ -0,0 +1,44 @@
+### Exception Handling ###
+
+numberOne = 5
+numberTwo = 1
+numberTwo = "1"
+
+# Flujo Básico de Excepción Base: try except
+try:
+    print(numberOne + numberTwo)
+    print("No se ha producido un error")
+except:
+    # Se ejecuta si se produce una excepción
+    print("Se ha producido un error")
+
+# Flujo Completo de Excepción: try except else finally
+try:
+    print(numberOne + numberTwo)
+    print("No se ha producido un error")
+except:
+    print("Se ha producido un error")
+else: # Opcional
+    # Se ejecuta si no se produce una excepción
+    print("La ejecución continúa correctamente")
+finally: # Opcional
+    # Se ejecuta siempre
+    print("La ejecución continúa")
+
+# Excepciones por tipo
+try:
+    print(numberOne + numberTwo)
+    print("No se ha producido un error")
+except ValueError:
+    print("Se ha producido un ValueError")
+except TypeError:
+    print("Se ha producido un TypeError")
+
+# Captura la información de la excepción
+try:
+    print(numberOne + numberTwo)
+    print("No se ha producido un error")
+except ValueError as error:
+    print(error)
+except Exception as my_random_error_name:
+    print(my_random_error_name)
---
+++ my_module.py
@@ -0,0 +1,7 @@
+### Módulo para Pruebas ###
+
+def sumValue(numberOne, numberTwo, numberThree):
+    print(numberOne + numberTwo+ numberThree)
+
+def printValue(value):
+    print(value)
